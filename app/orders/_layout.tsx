7e64fcf6a060e74765926e885e5e4ba5cb8de294
NRamosD <nixonramosdue@gmail.com>
2025-08-28 00:03:10 -0500

    vistas para cerrar flujo de toma de orden

--- app/orders/_layout.tsx
+++ app/orders/_layout.tsx
@@ -3,11 +3,15 @@ import { Stack } from 'expo-router';
 export default function OrdersLayout() {
   return <Stack>
     <Stack.Screen name='create-order' options={{
-      animation:"fade_from_bottom",
+      animation:"slide_from_right",
       headerShown:false
     }}/>
     <Stack.Screen name='checkout' options={{
       animation:"slide_from_right",
+      headerShown:false
+    }}/>
+    <Stack.Screen name='final-status-checkout' options={{
+      animation:"slide_from_bottom",
       headerShown:false
     }}/>
   </Stack>;
--- app/orders/final-status-checkout.tsx
+++ app/orders/final-status-checkout.tsx
@@ -5,6 +5,7 @@ import FloatingButton from '@/components/FloatingButton'
 import { ItemOrderSelected } from '@/components/orders'
 import { Ionicons } from '@expo/vector-icons'
 import { router } from "expo-router";
+import {useNavigation} from '@react-navigation/native';
 import React, { useEffect, useState } from 'react'
 import { FlatList, StyleSheet, TouchableOpacity } from 'react-native'
 import { Product } from '@/interfaces'
@@ -24,7 +25,10 @@ const data:Product[] = Array.from({ length: 20 }, (_, i) => ({
   fechaCreacion:new Date(),
 }) as Product );
 
-const CheckoutOrder = (props: Props) => {
+const FinalStatusCheckout = (props: Props) => {
+
+  const navigation = useNavigation();
+
   const [dataTest, setdataTest] = useState<Product[]>(data||[]);
   const [dataSelected, setDataSelected] = useState<Product[]>([]);
   const [textSearchedItem, setTextSearchedItem] = useState<string>("");
@@ -62,30 +66,13 @@ const CheckoutOrder = (props: Props) => {
 
 
   return (
-    <CContainerView style={{flex:1}}>
-      <CView style={{flex:1.5, backgroundColor:"#acacac", justifyContent:"center", height:20 }}>
-        <CText type="title" style={{ textAlign:"center", color:"white"}}>Resumen de la Orden</CText>
-      </CView>
-      <CView style={{flex:12, flexDirection:"row", zIndex:0, overflow:'hidden'}}>
-        <FlatList<Product>
-          data={dataSelected}
-          renderItem={({item}) => <ItemOrderSelected singleProduct={item} removeItem={()=>deleteItemSelected(item)} /> }
-          keyExtractor={item => item.uuid}
-          style={{height:"100%", width:"100%"}}
-        />
-      </CView>
+    <CContainerView style={{flex:1, justifyContent:"center"}}>
 
-      <CView style={{flex:2, flexDirection:"row", gap:15,
-        justifyContent:"flex-start", alignItems:"center", backgroundColor:"#1c1c1c"}}>
-          <CText type="title" style={{color:"white"}}>Total</CText>
-          <CText type="title" style={{color:"white"}}>$55.25</CText>
-
+      <CView style={{flex:6, gap:15, justifyContent:"center", paddingHorizontal:5, alignItems:"center", backgroundColor:"#1c1c1c"}}>
+          <CText type="title" style={{color:"white", textAlign:"center"}}>La orden ha sido facturada!</CText>
       </CView>
-
-      <CView style={{flex:2, flexDirection:"row", gap:15,
-        justifyContent:"flex-start", alignItems:"center", paddingHorizontal:10 }}>
-          <CButton onPress={()=>router.push({pathname:"/orders/final-status-checkout"})} title={"FACTURAR"} containerStyles={styles.touchableCreate}/>
-        
+      <CView style={{flex:1, justifyContent:"center", paddingHorizontal:10}}>
+        <CButton containerStyles={styles.touchableCreate} onPress={()=>router.dismissTo("/")} title={"Volver al inicio"}/>
       </CView>
       
 
@@ -94,7 +81,7 @@ const CheckoutOrder = (props: Props) => {
   )
 }
 
-export default CheckoutOrder
+export default FinalStatusCheckout
 
 const styles = StyleSheet.create({
   touchableCreate:{
